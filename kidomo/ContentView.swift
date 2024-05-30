181550da7195a1b56f1f2df918f26463fa90d047
Qubo Qin <quboqin0710@gmail.com>
2024-05-30 17:04:19 +0800

    Initial Commit

---
+++ kidomo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kidomo
+//
+//  Created by qinqubo on 2024/5/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kidomo/kidomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidomoApp.swift
+//  kidomo
+//
+//  Created by qinqubo on 2024/5/30.
+//
+
+import SwiftUI
+
+@main
+struct kidomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
